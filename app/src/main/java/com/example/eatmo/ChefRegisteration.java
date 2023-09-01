6bff62d2eb7ed6a85f35e1ee78be03ef1b3b82b2
udithasathsara <sathsarajmu0077@gmail.com>
2023-09-02 02:17:49 +0530

    first commit

---
+++ app/src/main/java/com/example/eatmo/ChefRegisteration.java
@@ -0,0 +1,14 @@
+package com.example.eatmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChefRegisteration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_registeration);
+    }
+}
---
+++ app/src/main/java/com/example/eatmo/Delivery_LoginPhone.java
@@ -0,0 +1,14 @@
+package com.example.eatmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Delivery_LoginPhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_login_phone);
+    }
+}
